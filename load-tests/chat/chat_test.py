f06b313e352152e32bb0df1f69539d8b26e13ff9
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2025-10-29 15:25:26 -0500

    Add: Chat Endpoint Tests

--- load-tests/chat/chat_test.py
+++ load-tests/chat/chat_test.py
@@ -11,25 +11,29 @@ load_dotenv()
 
 
 class ChatUser(HttpUser):
-    
-    agent_id = os.getenv["AGENT_ID"]
 
     @task
     def ask_agent(self):
+
+        agent_id = os.getenv("AGENT_ID")
+
+        headers = {
+            "Host": f"agent_{agent_id}",
+            "Content-Type": "application/json"
+        }
 
         chat_payload = {
             "question": "Considerando la complejidad de la nómina de Kluuvin Apteekki y la preocupación de Pia por perder el control y la seguridad de sus datos, ¿de qué manera un sistema de contabilidad en la nube (AIS) de tipo SaaS público podría simultáneamente solucionar su problema de carga administrativa y agravar sus mayores temores sobre la externalización y la tecnología?"
         }
 
         logging.info(f"Starting interaction with chat")
-        with self.client.post(f"/agent?agentID={self.agent_id}", json = chat_payload, catch_response = True) as resp:
+        with self.client.post(f"/ask", json = chat_payload, headers=headers, catch_response = True) as resp:
 
             if resp.status_code in (200, 201):
-                logging.info(f"Interaction with chat completed successfully")
                 resp.success()
+                logging.info(f"Interaction with chat completed successfully")
             else:
                 resp.failure(f"Error creating agent: {resp.status_code}")
-                return
         
         self.environment.runner.quit()
         return
