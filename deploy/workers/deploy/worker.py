7390dde38e46d5781f2ec33ea38f3b205b642fb7
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2025-10-29 23:27:37 -0500

    Update: Code for Chat Load Testing

--- deploy/workers/deploy/worker.py
+++ deploy/workers/deploy/worker.py
@@ -40,29 +40,29 @@ async def callback(message):
         async with await anyio.open_file(prompt_path, "r") as f:
             PROMPT += await f.read()
 
-        #await asyncio.create_subprocess_exec(
-        #    "docker", 
-        #    "run", 
-        #    "-d", 
-        #    "--name", container_name, 
-        #    "--network", "deplo_default",
-        #    "-e", f"AGENT_ID={agent_id}", 
-        #    "-e", f"GOOGLE_API_KEY={GOOGLE_API_KEY}", 
-        #    "-e", f"PROMPT={PROMPT}",
-        #    "-e", f"VIRTUAL_HOST={container_name}",
-        #    "-e", f"VIRTUAL_PORT={container_port}",
-        #    "-p", f"{host_port}:{container_port}", 
-        #    "-v", f"{host_path}:{container_path}", 
-        #    image_name,
-        #    stdout=asyncio.subprocess.PIPE,
-        #    stderr=asyncio.subprocess.PIPE,)
+        await asyncio.create_subprocess_exec(
+            "docker", 
+            "run", 
+            "-d", 
+            "--name", container_name, 
+            "--network", "deplo_default",
+            "-e", f"AGENT_ID={agent_id}", 
+            "-e", f"GOOGLE_API_KEY={GOOGLE_API_KEY}", 
+            "-e", f"PROMPT={PROMPT}",
+            "-e", f"VIRTUAL_HOST={container_name}",
+            "-e", f"VIRTUAL_PORT={container_port}",
+            "-p", f"{host_port}:{container_port}", 
+            "-v", f"{host_path}:{container_path}", 
+            image_name,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,)
         
-        message = {
-            "agent_id": agent_id,
-            "end_time": time.time()
-        }
+        #message = {
+        #    "agent_id": agent_id,
+        #    "end_time": time.time()
+        #}
 
-        await rabbitmq.publish("create_agent_test", json.dumps(message))
+        #await rabbitmq.publish("create_agent_test", json.dumps(message))
         
         logging.info(f"Agent deployed in http://localhost:{host_port} with ID {agent_id}")
 
--- load-tests/chat/load_test.py
+++ load-tests/chat/load_test.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant
 from dotenv import load_dotenv
 import logging
 import os
@@ -9,16 +9,24 @@ logging.basicConfig(level = logging.INFO, format = "%(asctime)s [%(levelname)s] 
 
 load_dotenv()
 
+AGENT_ID = os.getenv("AGENT_ID")
 
 class ChatUser(HttpUser):
+
+    wait_time = constant(10)
+    current_requests = 0
+    max_requests = 20
 
     @task
     def ask_agent(self):
 
-        agent_id = os.getenv("AGENT_ID")
+        if self.current_requests >= self.max_requests:
+            logging.info(f"Test ran {self.max_requests} times. Stopping")
+            self.environment.runner.quit()  
+            return
 
         headers = {
-            "Host": f"agent_{agent_id}",
+            "Host": f"agent_{AGENT_ID}",
             "Content-Type": "application/json"
         }
 
@@ -26,15 +34,14 @@ class ChatUser(HttpUser):
             "question": "Considerando la complejidad de la nómina de Kluuvin Apteekki y la preocupación de Pia por perder el control y la seguridad de sus datos, ¿de qué manera un sistema de contabilidad en la nube (AIS) de tipo SaaS público podría simultáneamente solucionar su problema de carga administrativa y agravar sus mayores temores sobre la externalización y la tecnología?"
         }
 
-        with self.client.post(f"/ask", json = chat_payload, headers=headers, catch_response = True) as resp:
+        with self.client.post(f"/ask", json = chat_payload, headers = headers, catch_response = True) as resp:
 
             if resp.status_code in (200, 201):
                 resp.success()
                 logging.info(f"Interaction with chat completed successfully")
             else:
-                resp.failure(f"Error creating agent: {resp.status_code}")
+                resp.failure(f"Error asking agent: {resp.status_code}")
         
-        self.environment.runner.quit()
-        return
+        self.current_requests += 1
 
 
